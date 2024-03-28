ca5f8a8251342f46c7877e316a75fe880952b8ba
Minura Dinupriya <mranaweera793@gmail.com>
2024-03-28 14:52:23 +0530

    update ui minor changes

--- scripts.js
+++ scripts.js
@@ -4,12 +4,12 @@ const addBtn = document.getElementById('addBtn');
 
 addBtn.addEventListener('click', function() {
     const li = document.createElement('li');
+    li.classList.add('list-group-item');
     const text = document.createTextNode(todoInput.value);
 
     const doneBtn = document.createElement('button');
-    doneBtn.textContent = 'Done'; // Corrected here
-    doneBtn.classList.add('done-btn');
-
+    doneBtn.textContent = 'Done';
+    doneBtn.classList.add('btn', 'btn-success', 'btn-sm', 'ms-3', 'float-right');
     doneBtn.addEventListener('click', function() {
         li.classList.toggle('completed');
     });
